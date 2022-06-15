e336934226e5e9afdc2f7739a0e7e50d57c2337e
nc-minh <nguyencongminh.official@gmail.com>
2022-06-15 22:02:48 +0700

    :wrench: Project Config

---
+++ src/apis/index.ts
@@ -0,0 +1,1 @@
+console.log(`apis`);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import noCache from 'nocache';
+
 const app = express();
+
+app.use(cors());
+
+function initializeSecurity() {
+  app.use(noCache());
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+}
+
+initializeSecurity();
+
 
 export default app;
