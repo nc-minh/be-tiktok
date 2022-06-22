48be94cfdc06b87185e49b1d05f946efb7e466f8
nc-minh <nguyencongminh.official@gmail.com>
2022-06-22 21:58:15 +0700

    update port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
